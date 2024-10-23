aeb2f1bb24bb8afaf71423437968c32049275623
Juan Cruz Montagni <juancruz.montagni@outlook.com>
2024-10-23 09:32:51 -0300

    realizamos ejercicio de pre-entrega

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-console.log("Hola Mundo")
+let nro = Number(prompt("Bienvenido al simulador de descuentos.\nEscriba el importe al que quiere aplicar el descuento."))
+let desc = Number(prompt("Qué descuento quiere aplicar? ecriba el numero y este será el porcentaje a aplicar."))
+let montoFinal = (nro - (nro * desc)/100)
+
+if (isNaN(nro)) {
+    alert("Debes ingresar un nro.")
+} 
+if (isNaN(desc)){
+    alert("No puedes ingresar caraceteres que no sean numeros.")
+}
+if (isNaN(montoFinal)) {
+    alert("Error, vuelve a intentar, escribe numeros y no letras.")
+} else {
+    alert("El importe es de: $" + nro + "\n" + "El descuento es de: $" + desc + "\n" + "El monto final es de: $" + montoFinal)
+}
