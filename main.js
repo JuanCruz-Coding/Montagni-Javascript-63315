d9c6948876b52d92f47c73ad0e4b6c3858634ec1
JuanCruz-Coding <juancruz.montagni@outlook.com>
2024-10-23 23:09:56 -0300

    codeamos las funciones "aumentarSalario"  y "calcularVacaciones" comprobando su funcionamiento

--- main.js
+++ main.js
@@ -1,15 +1,62 @@
-let nro = Number(prompt("Bienvenido al simulador de descuentos.\nEscriba el importe al que quiere aplicar el descuento."))
-let desc = Number(prompt("Qué descuento quiere aplicar? ecriba el numero y este será el porcentaje a aplicar."))
-let montoFinal = (nro - (nro * desc)/100)
+// programa que ayude a una empresa a incrementar los salarios de los trabajadores de la siguiente manera:
+// Tipo de salario Incremento Salarial
+// [ % ] De 0 hasta $9000 ---- aumenta 20%
+// De $9000 hasta $15000 ---- aumenta 10%
+// De $15000 hasta $20000 ---- aumenta 5%
+// Mas de $20000 3%
+let opc
+do {
+  opc = (prompt("---------------------------BIENVENIDO/A!---------------------------\n Seleccione una opcion colocando el numero correspondiente:\n1 - Para aumentar un salario\n2 - Para calcular vacaciones\n3 - Para calcular aguinaldo\n0 - Para Salir"))
+  switch (opc) {
+    case "1":
+      aumentarSalario();
+      break;
+    case "2":
+      calcularVacaciones();
+      break;
+    case "3":
+      // calcularAguinaldo();
+      break;
+    case "0":
+      break;
+    default:
+      alert("Opcion no valida")
+      break;
+  }
+} while (opc != 0);
 
-if (isNaN(nro)) {
-    alert("Debes ingresar un nro.")
-} 
-if (isNaN(desc)){
-    alert("No puedes ingresar caraceteres que no sean numeros.")
+function aumentarSalario(){
+  let cargo = prompt("Ingrese el cargo jerarquico\n1 - Director.\n2 - Gerente.\n3 - Operario.\n4 - Auxiliar.")
+
+  let salario = Number(prompt("Ingrese el salario."))
+  let salarioAumentado
+
+  if (cargo = "1"){
+    aumento = 0.03
+    salarioAumentado = salario + salario * aumento
+  } else if (cargo = "2"){
+    aumento = 0.05
+    salarioAumentado = salario + salario * aumento
+  } else if (cargo = "3"){
+    aumento = 0.10
+    salarioAumentado = salario + salario * aumento
+  } else if (cargo = "4"){
+    aumento = 0.2
+    salarioAumentado = salario + salario * aumento
+  }
+  return alert(`El salario aumentará por un importe de ${salario * aumento}.\nPasará a cobrar ${salarioAumentado}`)
 }
-if (isNaN(montoFinal)) {
-    alert("Error, vuelve a intentar, escribe numeros y no letras.")
-} else {
-    alert("El importe es de: $" + nro + "\n" + "El descuento es de: $" + desc + "\n" + "El monto final es de: $" + montoFinal)
+
+function calcularVacaciones(){
+  antiguedad = Number(prompt("Ingrese la antiguedad del empleado en años."))
+  if (antiguedad <= 5) {
+    alert("Corresponden 14 dias corridos de vacaciones")
+  } else if (antiguedad > 5 && antiguedad <= 10) {
+    alert("Corresponden 21 dias corridos de vacaciones")
+  } else if (antiguedad > 10 && antiguedad <= 20) {
+    alert("Corresponden 28 dias corridos de vacaciones")
+  } else if (antiguedad > 20) {
+    alert("Corresponden 35 dias corridos de vacaciones")
+  }
 }
+
